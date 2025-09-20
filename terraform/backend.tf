00a986d473353ca2a8f7d1b3e7e082dbd2f9aa0c
Jo Yu Sung <cys990617@gmail.com>
2025-09-20 15:57:41 +0900

    qwerqqqqq

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,8 +3,8 @@
 # Terraform Backend
 terraform {
   backend "s3" {
-    bucket         = "my-tfstate-bucket-test-1234511"
-    key            = "global/s3/terraform.tfstate"
+    bucket         = "terraform-git-whdbtjd"
+    key            = "cicd/s3/terraform.tfstate"
     region         = "ap-northeast-2"
     encrypt        = true
   }
