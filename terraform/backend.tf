69973741bda7694985a6d0fd38111f4d396b3b30
Jo Yu Sung <cys990617@gmail.com>
2025-09-20 15:50:14 +0900

    backend 버킷이름 난수 적용

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,11 @@
+# 버킷 이름 중복 방지용 랜덤 문자열
+resource "random_id" "tf_state_suffix" {
+  byte_length = 4
+}
+
 # S3 버킷 (Terraform state 저장)
 resource "aws_s3_bucket" "tf_state" {
-  bucket = "my-tfstate-bucket"
+  bucket = "my-tfstate-bucket-${random_id.tf_state_suffix.hex}"
 
   tags = {
     Name = "terraform-state"
@@ -9,7 +14,7 @@ resource "aws_s3_bucket" "tf_state" {
 
 # DynamoDB 테이블 (State Lock)
 resource "aws_dynamodb_table" "tf_locks" {
-  name         = "terraform-locks"
+  name         = "terraform-locks-${random_id.tf_state_suffix.hex}"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
@@ -26,10 +31,10 @@ resource "aws_dynamodb_table" "tf_locks" {
 # Terraform Backend
 terraform {
   backend "s3" {
-    bucket         = "my-tfstate-bucket"
+    bucket         = "my-tfstate-bucket-${random_id.tf_state_suffix.hex}"
     key            = "global/s3/terraform.tfstate"
     region         = "ap-northeast-2"
-    dynamodb_table = "terraform-locks"
+    dynamodb_table = "terraform-locks-${random_id.tf_state_suffix.hex}"
     encrypt        = true
   }
 }
