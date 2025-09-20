41cd244e8ebf38a21b92757854b9ddb330154b12
Jo Yu Sung <cys990617@gmail.com>
2025-09-20 15:55:34 +0900

    락기능삭제

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,19 +1,4 @@
 
-# DynamoDB 테이블 (State Lock)
-resource "aws_dynamodb_table" "tf_locks" {
-  name         = "terraform-locks-${random_id.tf_state_suffix.hex}"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags = {
-    Name = "terraform-locks"
-  }
-}
 
 # Terraform Backend
 terraform {
@@ -21,7 +6,6 @@ terraform {
     bucket         = "my-tfstate-bucket-test-1234511"
     key            = "global/s3/terraform.tfstate"
     region         = "ap-northeast-2"
-    dynamodb_table = "terraform-locks-${random_id.tf_state_suffix.hex}"
     encrypt        = true
   }
 }
