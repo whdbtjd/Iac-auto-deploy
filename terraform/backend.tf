287800def104a9734a61792b64d88e1e787164ac
Jo Yu Sung <cys990617@gmail.com>
2025-09-20 15:53:40 +0900

    backend수정

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,16 +1,3 @@
-# 버킷 이름 중복 방지용 랜덤 문자열
-resource "random_id" "tf_state_suffix" {
-  byte_length = 4
-}
-
-# S3 버킷 (Terraform state 저장)
-resource "aws_s3_bucket" "tf_state" {
-  bucket = "my-tfstate-bucket-${random_id.tf_state_suffix.hex}"
-
-  tags = {
-    Name = "terraform-state"
-  }
-}
 
 # DynamoDB 테이블 (State Lock)
 resource "aws_dynamodb_table" "tf_locks" {
@@ -31,7 +18,7 @@ resource "aws_dynamodb_table" "tf_locks" {
 # Terraform Backend
 terraform {
   backend "s3" {
-    bucket         = "my-tfstate-bucket-${random_id.tf_state_suffix.hex}"
+    bucket         = "my-tfstate-bucket-test-1234511"
     key            = "global/s3/terraform.tfstate"
     region         = "ap-northeast-2"
     dynamodb_table = "terraform-locks-${random_id.tf_state_suffix.hex}"
