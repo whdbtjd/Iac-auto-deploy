6632f34d5ea3b7a539137272fffcd012b002d88b
Jo Yu Sung <cys990617@gmail.com>
2025-09-20 20:43:11 +0900

    db연결 오류 디버깅용 단계 추가

--- backend/src/main/java/com/example/iac_sample_app/controller/VoteController.java
+++ backend/src/main/java/com/example/iac_sample_app/controller/VoteController.java
@@ -8,7 +8,7 @@ import java.util.*;
 @CrossOrigin(origins = "http://localhost:3000")
 public class VoteController {
 
-    // 임시 데이터 (실제로는 DB 연동) asdfasdfasdf
+    // 임시 데이터 (실제로는 DB 연동) asdfasdfasdfasd
     private List<Map<String, Object>> votes = new ArrayList<>();
     private Long nextId = 1L;
 
