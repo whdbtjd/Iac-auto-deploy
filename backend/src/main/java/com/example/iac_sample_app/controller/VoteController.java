6b88dd95e3423d80d6e06876b9a2e5c28848fe15
Jo Yu Sung <cys990617@gmail.com>
2025-09-21 20:44:00 +0900

    build.gradle에 의존성 추가

--- backend/src/main/java/com/example/iac_sample_app/controller/VoteController.java
+++ backend/src/main/java/com/example/iac_sample_app/controller/VoteController.java
@@ -8,7 +8,7 @@ import java.util.*;
 @CrossOrigin(origins = "http://localhost:3000")
 public class VoteController {
 
-    // 임시 데이터 (실제로는 DB 연동) asdasdfddasaaadf
+    // 임시 데이터 (실제로는 DB 연동) asdasdfddasaaadfadad
     private List<Map<String, Object>> votes = new ArrayList<>();
     private Long nextId = 1L;
 
