7622c1c32d951146b06b076d1aa6b8a19105560b
Jo Yu Sung <cys990617@gmail.com>
2025-09-21 21:10:01 +0900

    ㄹㅇ 되는지 찐막 테스트

--- backend/src/main/java/com/example/iac_sample_app/controller/VoteController.java
+++ backend/src/main/java/com/example/iac_sample_app/controller/VoteController.java
@@ -8,7 +8,7 @@ import java.util.*;
 @CrossOrigin(origins = "http://localhost:3000")
 public class VoteController {
 
-    // 임시 데이터 (실제로는 DB 연동) asdasdfddasaaadfadadaasdf
+    // 임시 데이터 (실제로는 DB 연동) asdasdfddasaaadfadadaasdfasdf
     private List<Map<String, Object>> votes = new ArrayList<>();
     private Long nextId = 1L;
 
