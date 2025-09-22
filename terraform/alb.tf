155fa874cb08e1f6ef9723f5b2b75bdce3e50e47
Jo Yu Sung <cys990617@gmail.com>
2025-09-22 17:07:00 +0900

    HTTP로 되돌림

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -23,7 +23,7 @@ resource "aws_lb_target_group" "alb-tg" {
 resource "aws_lb_listener" "alb-listner" {
   load_balancer_arn = aws_lb.alb.arn
   port       = "80"
-  protocol   = "HTTPS"
+  protocol   = "HTTP"
 
   default_action{
     type              = "forward"
