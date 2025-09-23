6c68e01de5b517ddaf9468bc12f511a7bc062e56
Jo Yu Sung <cys990617@gmail.com>
2025-09-23 20:35:15 +0900

    백엔드부터 배포

--- backend/src/main/java/com/example/iac_sample_app/controller/ResourceController.java
+++ backend/src/main/java/com/example/iac_sample_app/controller/ResourceController.java
@@ -22,7 +22,7 @@ public class ResourceController {
     private final AwsResourceService awsResourceService;
 
     /**
-     * 전체 인프라 상태 조회 (데모용 메인 API)ㅁㄴㅇㄹㅁㅁㅁㅁㅁddasdf
+     * 전체 인프라 상태 조회 (데모용 메인 API)ㅁㄴㅇㄹㅁㅁㅁㅁㅁddasdfasdf
      */
     @GetMapping("/status")
     public ResponseEntity<InfrastructureStatusResponse> getInfrastructureStatus() {
