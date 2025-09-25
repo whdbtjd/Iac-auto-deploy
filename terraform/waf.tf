e12a1c90477cc4193952ed03b1fbb6220bb41ed6
Jo Yu Sung <cys990617@gmail.com>
2025-09-25 15:41:32 +0900

    WAF적용중

--- terraform/waf.tf
+++ terraform/waf.tf
@@ -1,4 +1,12 @@
+# CloudFront용 WAF는 반드시 us-east-1에서 생성
+provider "aws" {
+  alias  = "us_east_1"
+  region = "us-east-1"
+}
+
 resource "aws_wafv2_web_acl" "cloudfront_waf" {
+  provider = aws.us_east_1  # 이 라인 추가
+  
   name  = "cloudfront-waf"
   scope = "CLOUDFRONT"
 
